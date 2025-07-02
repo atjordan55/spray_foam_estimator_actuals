3ae4a566f2d4d492f1be813729d0e5761aea1799
atjordan55 <37186192+atjordan55@users.noreply.github.com>
2025-07-02 09:20:14 -0400

    Add files via upload
    Placeholder Fix

--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import SprayFoamEstimator from './App.jsx';
+import ReactDOM from 'react-dom/client';
+import App from './App';
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   <React.StrictMode>
-    <SprayFoamEstimator />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <App />
+  </React.StrictMode>
 );
