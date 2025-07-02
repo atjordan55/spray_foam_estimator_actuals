1cf25257af39b12d6bbd091b3ae3f15b152f68b1
atjordan55 <37186192+atjordan55@users.noreply.github.com>
2025-07-02 09:28:47 -0400

    Add files via upload
    Back to deployed version

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+import ReactDOM from 'react-dom';
+import './index.css';
+import SprayFoamEstimator from './App.jsx';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+ReactDOM.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <SprayFoamEstimator />
+  </React.StrictMode>,
+  document.getElementById('root')
 );
